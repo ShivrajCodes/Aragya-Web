903fd76df6e6b18202f98c02eccbd09e3ac1ee1f
Shivraj <124032075+ShivrajCodes@users.noreply.github.com>
2024-10-04 05:35:17 +0530

    OK

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -10,7 +10,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-custom-purple rounded-lg shadow-lg max-w-4xl w-full p-6 relative"> {/* Increased max width */}
+      <div className="bg-custom-purple rounded-lg shadow-lg max-w-4xl w-full p-6 relative"> 
         <button
           onClick={onClose}
           className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
